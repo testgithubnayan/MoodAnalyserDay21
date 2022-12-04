c6612e29ec1457a4ff1fba4cfa8fcf801ee61a8d
NAYAN S. SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-12-04 22:37:21 +0530

    Create MoodAnalyser.java

---
+++ src/main/MoodAnalyser.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.moodanalyser;
+
+public class MoodAnalyser {
+    String message;
+
+    public MoodAnalyser(String message){
+        this.message = message;
+    }
+    public MoodAnalyser() {
+    }
+
+    public String analyseMood() throws MoodAnalyserException {
+        try {
+            if (message.toLowerCase().contains("sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        }
+        catch (Exception e){
+            throw new MoodAnalyserException(MoodAnalyserException.exceptionType.NULL,("Message is Null"));
+        }
+    }
+}
